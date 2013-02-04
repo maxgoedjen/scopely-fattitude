f30cb3cee3b091eb7ed229fc4854b9d04f5914ce
Max Goedjen <max.goedjen@gmail.com>
2013-02-04 01:12:04 -0800

    Ranking

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
-function percentChanges(data) {
+function percentChanges(people) {
 	var parsed = [];
-	for (var x=0;x<data.length;x++) {
-		var person = data[x];
+	for (var x=0;x<people.length;x++) {
+		var person = people[x];
 
 		var endObject = {"name" : person["name"]};
 		var rawData = person["data"];
@@ -18,9 +18,6 @@ function percentChanges(data) {
 	}
 
 	var sorted = parsed.sort(percentSorter);
-	for (var i=0; i<sorted.length; i++) {
-		console.log(i + ' : ' + sorted[i]['name']);
-	}
 	return sorted;
 }
 
@@ -28,8 +25,8 @@ function percentDifference(first, current) {
 	return 100.0-(100.0/(first/current));
 }
 
-function currentPercentDifference(data) {
-	var points = data["data"];
+function currentPercentDifference(person) {
+	var points = person["data"];
 	return points[points.length-1];
 }
 
@@ -45,6 +42,59 @@ function percentSorter(a, b) {
 	}
 }
 
-function rankedBettors(data, bets) {
-	
+function rankedBettors(weightRankings, bettors) {
+	var rankedBettors = [];
+	for (var i=0; i<bettors.length; i++) {
+		var ranked = rankedBettor(weightRankings, bettors[i]);
+		rankedBettors.push(ranked);
+	}
+	var sorted = rankedBettors.sort(netSorter);
+	return sorted;
+}
+
+function rankedBettor(weightRankings, bettorData) {
+	var ranked = {'name' : bettorData['name']};
+	var bets = bettorData['bets'];
+	var rankedBets = []
+	for (var i=0; i<bets.length; i++) {
+		var index = -1;
+		for (var x=0; x<weightRankings.length; x++) {
+			if (weightRankings[x]['name'] == bets[i]) {
+				index = x;
+			}
+		}
+		if (index == -1) {
+			console.log('ERROR: ' + bets[i]);
+		}
+		var diff = Math.abs(index - i);
+		rankedBets.push({
+			'name' : bets[i],
+			'diff' : diff
+		});
+	}
+	ranked['bets'] = rankedBets;
+	ranked['net'] = netBettor(ranked);
+	return ranked;
+}
+
+function netBettor(bettor) {
+	var bets = bettor['bets'];
+	var net = 0;
+	for (var i=0; i<bets.length; i++) {
+		var bet = bets[i];
+		net += bet['diff'];
+	}
+	return net;
+}
+
+function netSorter(a, b) {
+	var netA = a['net'];
+	var netB = b['net'];
+	if (netA == netB) {
+		return 0;
+	} else if (netA < netB) {
+		return -1;
+	} else {
+		return 1;
+	}
 }
--- graph.js
+++ graph.js
@@ -1,70 +1,50 @@
 $(function () {
-	var chart;
 	$(document).ready(function() {
-		
-		var xhr = new XMLHttpRequest();
-		  try {
-			xhr.onreadystatechange = function(){
-			  if (xhr.readyState != 4)
-				return;
-		
-			  if (xhr.responseText) {
-				jsonData = percentChanges(JSON.parse(xhr.responseText));
-				
-				chart = new Highcharts.Chart({
-					chart: {
-						renderTo: 'graphContainer',
-						type: 'line',
-						marginRight: 130,
-						marginBottom: 25
-					},
+		$.getJSON('data_weighins.json', function(jsonData) {
+			var parsed = percentChanges(jsonData);
+			var chart = new Highcharts.Chart({
+				chart: {
+					renderTo: 'graphContainer',
+					type: 'line',
+					marginRight: 130,
+					marginBottom: 25
+				},
+				title: {
+					text: 'Scopely Weight Loss Challenge',
+					x: -20
+				},
+				xAxis: {
+					categories: ['1', '2', '3', '4', '5', '6',
+						'7', '8', '9', '10', '11', '12']
+				},
+				yAxis: {
 					title: {
-						text: 'Scopely Weight Loss Challenge',
-						x: -20
+						text: '% of Fat Percentage Lost'
 					},
-					xAxis: {
-						categories: ['1', '2', '3', '4', '5', '6',
-							'7', '8', '9', '10', '11', '12']
-					},
-					yAxis: {
-						title: {
-							text: '% of Fat Percentage Lost'
-						},
-						plotLines: [{
-							value: 0,
-							width: 1,
-							color: '#808080'
-						}]
-					},
-					tooltip: {
-						formatter: function() {
-								return '<b>'+ this.series.name +'</b><br/>'+
-								Math.round(this.y) +'% Change';
-						}
-					},
-					legend: {
-						layout: 'vertical',
-						align: 'right',
-						verticalAlign: 'top',
-						x: -10,
-						y: 100,
-						borderWidth: 0
-					},
-					series: jsonData
-				});
+					plotLines: [{
+						value: 0,
+						width: 1,
+						color: '#808080'
+					}]
+				},
+				tooltip: {
+					formatter: function() {
+							return '<b>'+ this.series.name +'</b><br/>'+
+							Math.round(this.y) +'% Change';
+					}
+				},
+				legend: {
+					layout: 'vertical',
+					align: 'right',
+					verticalAlign: 'top',
+					x: -10,
+					y: 100,
+					borderWidth: 0
+				},
+				series: parsed
+			});
 
-			  }
-			}
-		
-			xhr.onerror = function(error) {
-			  console.debug(error);
-			}
-		
-			xhr.open("GET", "data_weighins.json", true);
-			xhr.send(null);
-		  } catch(e) {
-			console.error(e);
-		  }		
+		});
 
 	});
 
