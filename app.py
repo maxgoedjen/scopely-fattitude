36f3169b3946de2aa8647c9e393f42381ee9ab02
Max Goedjen <max.goedjen@gmail.com>
2013-03-07 12:31:46 -0800

    New data

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def css(filename):
 
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
-	port = int(os.environ.get('PORT', 5000))
+	port = int(os.environ.get('PORT', 5002))
 	run(host='0.0.0.0', port=port)
